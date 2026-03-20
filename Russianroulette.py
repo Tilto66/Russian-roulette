13e8e2f186ce3e59170cebf06f8836e147b516df
Tilto66 <antoninvallonazoulay@gmail.com>
2026-03-20 16:03:29 +0100

    Tilto66: Implement signal handling in Russianroulette.py
    Added signal handling for graceful termination.

--- Russianroulette.py
+++ Russianroulette.py
@@ -1,9 +1,14 @@
-import random, os
+import random, os, pywin32, signal
 
 
 number = random.randint(1,100)
 guess = input("Guess a number between 1 and a 100")
 guess = int(guess)
+
+def handler(signum, frame):
+  signal.signal(signal.SIGINT, handler)
+while True:
+    pass
 
 if guess == number:
   print ("You won !")

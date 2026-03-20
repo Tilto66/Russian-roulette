63366091e022a0d5c2e7d7681036cfc021b825dc
Tilto66 <antoninvallonazoulay@gmail.com>
2026-03-20 16:06:16 +0100

    Tilto66: Refactor imports and add console close handler
    Updated imports and added signal handling for console close events.

--- Russianroulette.py
+++ Russianroulette.py
@@ -1,4 +1,4 @@
-import random, os, pywin32, signal
+import random, os, win32api, signal
 
 
 number = random.randint(1,100)
@@ -6,10 +6,18 @@ guess = input("Guess a number between 1 and a 100")
 guess = int(guess)
 
 def handler(signum, frame):
+  print("Non")
   signal.signal(signal.SIGINT, handler)
 while True:
     pass
-
+  
+def on_close(event):
+    print("Non")
+    return True  # empêche la fermeture
+win32api.SetConsoleCtrlHandler(on_close, True)
+while True:
+    pass
+  
 if guess == number:
   print ("You won !")
 else:

5948b56dcf9938421520fa6ceda490899bd2eca8
Tilto66 <antoninvallonazoulay@gmail.com>
2026-04-17 14:40:37 +0200

    Tilto66: Enhance Run.bat with compile and execute options
    Added user prompts for compiling and executing the main script.

--- Run.bat
+++ Run.bat
@@ -1,8 +1,24 @@
 @echo off
-echo Installation in progress
 cd /d %~dp0
+echo Installation in progress 
 python -m pip install -r requirements.txt
 echo Installation completed
+
+
+:question
+set /p comp=Do you want to compile it ? (y/n) : 
+if /i "%comp%"=="y" goto yes
+if /i "%comp%"=="n" goto no
+
+:yes
+python -m pip install pyinstaller
+python -m PyInstaller --onefile Russianroulette.py 
+echo Done ! 
+pause
+goto question
+
+
+:no
 set /p choix=Execute main.py ? (y/n) : 
 if /i "%choix%"=="y" (
     python main.py
